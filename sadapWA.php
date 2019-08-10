915d641d67cf571fd1561cca229d45cd58d11e05
MR-B95dCyb3r <53963731+MR-B95dCyber@users.noreply.github.com>
2019-08-10 14:57:08 +0700

    SadapWA

---
+++ sadapWA.php
@@ -0,0 +1,54 @@
+<?php
+system('clear');
+echo "\e[1;36m
+﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉";
+echo "\e[1;32m
+Author : MR.B95d_Cyb3r
+IG     : Wahyudi_programing
+Team   : Sunda Cyber Team";
+echo "\e[1;36m
+﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉﹉\n";
+echo "\e[1;36mGUNAKAN TOOLS IN DENGAN BIJAK/MR.B95d\n";
+echo "
+
+";
+echo "Masukan no kamu   :";
+$spill = trim(fgets(STDIN));
+echo "Masukan no target :";
+$spill = trim(fgets(STDIN));
+sleep(2);
+echo "[+] syncing target....\n";
+sleep(3);
+echo "LogIn ==> +628316014955\n";
+sleep(3);
+echo "target ==>+6287769723882\n";
+echo "[+] sedang masuk....\n";
+sleep(5);
+echo "[+] Done.
++6281947637584
++6282307486626
++6285367115813
++6283150316926
++6283176468928\n";
+echo "[✓] Open chatting :";
+$spill = trim(fgets(STDIN));
+sleep(2);
+echo "
+senin,2019/08/01 Hendara : p
+senin,2019/08/01  Intan Adja : iya 
+selasa,2019/08/02 Hendra : Gimana besok? 
+selasa,2019/08/02 Intan Adja : ya, jadi lah
+selasa,2019/08/02 Hendra : ooo, hars bw ap? 
+Selena,2019/08/02 Intan Adja : bw baju atribt,
+selasa,2019/08/02 Hendra : la si dani bw ap?
+rabu,2019/08/03 Intan Adja : dia sekln bwa jg 
+rabu,2019/08/03 Hendra : iya, Oww....
+rabu,2019/08/03 Hendra : dimana tmpt msng
+rabu,2019/08/03 Intan Adja : nanti ad pngws
+rabu,2019/08/03 Hendra : ok, lah ya..
+rabu,2019/08/03 Hendra : moga juara hehe 
+rabu,2019/08/03 Intan Adja : amin dra..
+rabu,2019/08/03 Hendra : ok.\n";
+?>
+
+
